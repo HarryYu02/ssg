3c148c11644ddbad7f5d832ec4112678ae8cd34f
Harry Y. <harryyu02@gmail.com>
2026-01-29 12:14:36 -0500

    add function to extraca links and images

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from utils import split_nodes_delimiter
+from utils import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 
@@ -70,6 +70,42 @@ class TestUtils(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" in the text", TextType.PLAIN),
         ])
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_images_none(self):
+        matches = extract_markdown_images(
+            "This is text without an image"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_images_mix(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_extract_markdown_links_none(self):
+        matches = extract_markdown_links(
+            "This is text without a link"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_links_mix(self):
+        matches = extract_markdown_links(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 
@@ -10,18 +12,28 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if old_node.text_type != TextType.PLAIN:
             new_nodes.append(old_node)
             continue
+
         splitted = old_node.text.split(delimiter)
         if len(splitted) < 3 or len(splitted) % 2 == 0:
             raise Exception("Error: node not properly enclosed")
-        new_splitted_nodes = []
+
         for i in range(0, len(splitted)):
-            splitted_text = splitted[i]
-            if len(splitted_text) == 0:
+            if len(splitted[i]) == 0:
                 continue
-            if i % 2 == 0:
-                new_splitted_nodes.append(TextNode(splitted_text, TextType.PLAIN))
-            else:
-                new_splitted_nodes.append(TextNode(splitted_text, text_type))
-        new_nodes.extend(new_splitted_nodes)
+            new_node = TextNode(splitted[i], text_type if i % 2 == 1 else TextType.PLAIN)
+            new_nodes.append(new_node)
 
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"\!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!\!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    pass
+
+def split_nodes_link(old_nodes):
+    pass
+
